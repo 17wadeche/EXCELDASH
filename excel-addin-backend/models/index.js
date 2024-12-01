bac4c331a2ce03dab71414440a986f7d08897638
Chey Wade <cwade1755@gmail.com>
2024-12-01 17:47:15 -0600

    Add README to excel-addin-backend as a regular directory

---
+++ excel-addin-backend/models/index.js
@@ -0,0 +1,39 @@
+const initializeSequelize = require('../db'); // Adjust the path as necessary
+
+const defineUser = require('./User');
+const defineLicense = require('./License');
+const defineSubscription = require('./Subscription');
+
+async function initializeModels() {
+  const sequelize = await initializeSequelize();
+
+  // Define models
+  const User = defineUser(sequelize);
+  const License = defineLicense(sequelize);
+  const Subscription = defineSubscription(sequelize);
+
+  // Define relationships
+  User.hasMany(License, { foreignKey: 'userId' });
+  License.belongsTo(User, { foreignKey: 'userId' });
+
+  License.hasOne(Subscription, { foreignKey: 'licenseId' });
+  Subscription.belongsTo(License, { foreignKey: 'licenseId' });
+
+  // Synchronize Models with Database
+  try {
+    await sequelize.sync();
+    console.log('All models were synchronized successfully.');
+  } catch (error) {
+    console.error('Unable to sync models:', error);
+    process.exit(1);
+  }
+
+  return {
+    sequelize,
+    User,
+    License,
+    Subscription,
+  };
+}
+
+module.exports = initializeModels;
