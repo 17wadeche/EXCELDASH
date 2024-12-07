c31d29e180bcc20c91ed1d0a8f65fc77ffbe3031
Chey Wade <cwade1755@gmail.com>
2024-12-06 19:14:05 -0600

    resize

--- frontend/src/taskpane/components/Dashboard.tsx
+++ frontend/src/taskpane/components/Dashboard.tsx
@@ -606,7 +606,7 @@ const Dashboard: React.FC<DashboardProps> = ({ isPresenterMode = false, closePre
         className={`dashboard-container ${theme}`}
         style={{
           ...borderStyle,
-          width:'700px',
+          width:'680px',
           height: 'auto',
           overflow: 'auto',
           paddingBottom: '3px',
