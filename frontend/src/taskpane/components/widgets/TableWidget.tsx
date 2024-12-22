0e40cfde318855fece4626afb8c2ec1ca9c4a17a
Chey Wade <cwade1755@gmail.com>
2024-12-21 23:07:43 -0600

    center table title

--- frontend/src/taskpane/components/widgets/TableWidget.tsx
+++ frontend/src/taskpane/components/widgets/TableWidget.tsx
@@ -33,27 +33,41 @@ const TableWidgetComponent: React.FC<TableWidgetProps> = ({
     <div style={{ width: '100%', height: '100%' }}>
       <div
         className="drag-handle"
-        style={{ cursor: 'move', marginBottom: 8, display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}
+        style={{
+          cursor: 'move',
+          marginBottom: 8,
+          display: 'flex',
+          alignItems: 'center',
+        }}
       >
         {onRefresh && (
           <Button size="small" onClick={() => onRefresh(id)}>
             Refresh
           </Button>
         )}
-        {isEditing ? (
-          <Input
-            autoFocus
-            value={title}
-            onChange={(e) => setTitle(e.target.value)}
-            onBlur={handleBlur}
-            onPressEnter={handleBlur}
-            style={{ width: '70%' }}
-          />
-        ) : (
-          <h3 style={{ cursor: 'pointer', margin: 0 }} onClick={() => setIsEditing(true)} >
-            {title}
-          </h3>
-        )}
+        <div style={{ flex: 1, display: 'flex', justifyContent: 'center' }}>
+          {isEditing ? (
+            <Input
+              autoFocus
+              value={title}
+              onChange={(e) => setTitle(e.target.value)}
+              onBlur={handleBlur}
+              onPressEnter={handleBlur}
+              style={{ width: '70%' }}
+            />
+          ) : (
+            <h3
+              style={{
+                cursor: 'pointer',
+                margin: 0,
+                textAlign: 'center',
+              }}
+              onClick={() => setIsEditing(true)}
+            >
+              {title}
+            </h3>
+          )}
+        </div>
       </div>
       <Table
         columns={data.columns as ColumnsType<any>}
