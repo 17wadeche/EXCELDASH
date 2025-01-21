9a2f064d0541de887cdc4bd87921e9da05a355de
Chey Wade <cwade1755@gmail.com>
2025-01-21 09:38:34 -0600

    scrapping width

--- frontend/src/taskpane/components/DashboardSettingsModal.tsx
+++ frontend/src/taskpane/components/DashboardSettingsModal.tsx
@@ -49,13 +49,6 @@ const DashboardSettingsModal: React.FC<DashboardSettingsModalProps> = ({ visible
       backgroundColor: color.hex,
     });
   };
-  const handleWidthChange = (value: number | null) => {
-    const newWidth = value && value > 0 ? value : 730;
-    setDashboardBorderSettings({
-      ...dashboardBorderSettings,
-      width: newWidth,
-    });
-  };
 
   return (
     <Modal
@@ -105,15 +98,6 @@ const DashboardSettingsModal: React.FC<DashboardSettingsModalProps> = ({ visible
         <SketchPicker
           color={dashboardBorderSettings.backgroundColor || '#ffffff'}
           onChange={handleBackgroundColorChange}
-        />
-      </div>
-      <div style={{ marginTop: 16 }}>
-        <span>Dashboard Width (px): </span>
-        <InputNumber
-          min={300}
-          max={3000}
-          value={dashboardBorderSettings.width}
-          onChange={handleWidthChange}
         />
       </div>
     </Modal>
