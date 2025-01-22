3a4c370f8f3fe1db6d939d4a9e6b423146a70658
Chey Wade <cwade1755@gmail.com>
2025-01-21 22:33:08 -0600

    2

--- api/email.js
+++ api/email.js
@@ -1,5 +1,3 @@
-// src/taskpane/utils/email.js
-
 const sgMail = require('@sendgrid/mail');
 
 sgMail.setApiKey(process.env.SENDGRID_API_KEY);
@@ -7,7 +5,7 @@ sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 const sendPasswordResetEmail = async (to, resetLink) => {
   const msg = {
     to,
-    from: process.env.EMAIL_FROM,
+    from: process.env.EMAIL_FROM, // Must be verified in SendGrid
     subject: 'Password Reset Request',
     text: `You requested a password reset. Click the link below to reset your password:\n\n${resetLink}\n\nIf you did not request this, please ignore this email.`,
     html: `
