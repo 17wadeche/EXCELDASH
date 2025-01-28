2b50e357dde7e9f06e6f611a593e579e03b74057
Chey Wade <cwade1755@gmail.com>
2025-01-27 19:45:18 -0600

    14

--- frontend/src/taskpane/components/Dashboard.tsx
+++ frontend/src/taskpane/components/Dashboard.tsx
@@ -367,7 +367,7 @@ const Dashboard: React.FC<DashboardProps> = React.memo(({ isPresenterMode = fals
     () =>
       debounce(async () => {
         if (!isSavingRef.current) {await handleSave();}
-      }, 2000),
+      }, 300000),
     [handleSave]
   );
 
@@ -731,7 +731,7 @@ const Dashboard: React.FC<DashboardProps> = React.memo(({ isPresenterMode = fals
                   checkedChildren="Auto-Save On"
                   unCheckedChildren="Auto-Save Off"
                   aria-label="Toggle Auto-Save"
-                  style={{ marginTop: '16px', width: '50px' }} // Adjust styling as needed
+                  style={{ marginTop: '16px', width: '30px' }} // Adjust styling as needed
                 />
               </Tooltip>
               <Tooltip title="Refresh All Charts" placement="left">
