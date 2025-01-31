f76df3b48e07a2237a64ef5fe2801aa7554d98b7
Chey Wade <cwade1755@gmail.com>
2025-01-31 11:33:29 -0600

    c

---
+++ frontend/src/chartjs-plugin-trendline.d.ts
@@ -0,0 +1,16 @@
+// src/types/chartjs-plugin-trendline.d.ts
+
+import 'chart.js';
+
+declare module 'chart.js' {
+  interface PluginOptionsByType<TType extends ChartType> {
+    trendlineLinear?: {
+      colorMin?: string;
+      colorMax?: string;
+      lineStyle?: string;
+      width?: number;
+      projection?: boolean;
+      [key: string]: any;
+    };
+  }
+}
